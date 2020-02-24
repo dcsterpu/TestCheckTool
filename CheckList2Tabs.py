c248519d7a9fd7747b94fafa7f4ec68b87380221
dcsterpu <dcsterpu>
2020-02-24 10:42:29 +0200

    fixed GUI dynamic generation

--- CheckList2Tabs.py
+++ CheckList2Tabs.py
@@ -18,11 +18,34 @@ from datetime import date
 # from openpyxl.styles import Alignment, Border, Side
 # import win32com.client as win32
 # import win32api
+
+
 files_path = []
-
-
-
 appName = "Check Tool"
+
+
+class LineEdit(QLineEdit):
+
+    def __init__(self, title, parent):
+        super().__init__(title, parent)
+
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        files = [str(u.toLocalFile()) for u in event.mimeData().urls()]
+        for f in files:
+            temp = self.text()
+            temp = temp + str(f) + "\n"
+            files_path.append(str(f))
+            self.setText(temp)
+            # self.setText(("\n"))
+
 
 class Application(QWidget):
 
@@ -32,7 +55,6 @@ class Application(QWidget):
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
         self.tab2 = QWidget()
-
         self.tabs.addTab(self.tab1, "Tab1")
         self.tabs.addTab(self.tab2, "Tab2")
         self.initUI(self.tab1)
@@ -41,10 +63,8 @@ class Application(QWidget):
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
         self.setWindowTitle(appName)
-
         self.list_document = []
-        # self.tab1.setEnabled(False)
-
+        self.correspondences = {}
 
     def initUI(self, tab):
 
@@ -60,7 +80,7 @@ class Application(QWidget):
         tab.button1.resize(50, 25)
         tab.button1.clicked.connect(self.openFileNameDialog1)
 
-        tab.button = QPushButton("Start check", tab)
+        tab.button = QPushButton("Import Checklist", tab)
         tab.button.move(250, 400)
         tab.button.clicked.connect(self.buttonGenerateClicked)
 
@@ -70,58 +90,11 @@ class Application(QWidget):
         fileName1, _filter = QFileDialog.getOpenFileName(self, 'Open File', QtCore.QDir.rootPath(), '*.*')
         self.tab1.edit1.setText(fileName1)
 
-    # def createFunctions(self):
-    #     for function in self.list_document:
-    #
-    #         exec('''def openFileNameDialog''' + function + '''(self):
-    #                 fileName''' + function + ''', _filter = QFileDialog.getOpenFileName(self, 'Open File', QtCore.QDir.rootPath(), '*.*')
-    #                 self.tab1.edit''' + function + '''.setText(''' + '\'' + '''fileName1'''+ function + '\'' + ''')''')
-
-
-    # def initUIOptions(self, tab):
-    #
-    #     # set controls for Test Plan
-    #     tab.label1 = QLabel("Test Plan", tab)
-    #     tab.label1.move(20, 35)
-    #     tab.edit1 = QLineEdit('', tab)
-    #     tab.edit1.setDragEnabled(False)
-    #     tab.edit1.move(120, 30)
-    #     tab.edit1.resize(430, 25)
-    #     tab.button1 = QPushButton("Browse", tab)
-    #     tab.button1.move(560, 30)
-    #     tab.button1.resize(50, 25)
-    #     # tab.button1.clicked.connect(self.openFileNameDialog1)
-
-    def createFunctions(self, data_list):
-
-
-        # DocPath = self.tab1.edit1.text()
-        # Workbook = openpyxl.load_workbook(DocPath)
-        # Sheet = Workbook['Test']
-        #
-        # self.list_document.append(Sheet['A1'].value)
-        # self.list_document.append(Sheet['A2'].value)
-        # self.list_document.append(Sheet['A3'].value)
-        # self.list_document.append(Sheet['A4'].value)
-        # self.list_document.append(Sheet['A5'].value)
-
-
-        for document_name in data_list:
-            exec('''def openFileNameDialog''' + document_name + '''(self):
-                                fileName''' + document_name + ''', _filter = QFileDialog.getOpenFileName(self, 'Open File', QtCore.QDir.rootPath(), '*.*')
-                                self.tab1.edit''' + document_name + '''.setText(''' + '\'' + '''fileName1''' + document_name + '\'' + ''')''')
-
-    # exec('''def openFileNameDialog''' + 'Test' + '''(self):
-    #                                 fileName''' + 'Test' + ''', _filter = QFileDialog.getOpenFileName(self, 'Open File', QtCore.QDir.rootPath(), '*.*')
-    #                                 self.tab1.edit''' + 'Test' + '''.setText(''' + '\'' + '''fileName1''' + 'Test' + '\'' + ''')''')
-
     def buttonGenerateClicked(self):
-
-        # self.createFunctions()
 
         DocPath = self.tab1.edit1.text()
         Workbook = openpyxl.load_workbook(DocPath)
-        Sheet = Workbook['Test']
+        Sheet = Workbook['Checks']
 
         self.list_document.append(Sheet['A1'].value)
         self.list_document.append(Sheet['A2'].value)
@@ -131,24 +104,25 @@ class Application(QWidget):
 
         y_label = 35
         y_edit = 30
-        self.createFunctions(self.list_document)
-
         for document_name in self.list_document:
             exec('self.tab2.label' + document_name + ' = QLabel(' + "\'" + document_name + "\'" + ', self.tab2)')
             exec('self.tab2.label' + document_name + '.move(20, ' + str(y_label) + ')')
-            exec('self.tab2.edit' + document_name + ' = QLineEdit(' + "\'" + "\'" + ', self.tab2)')
+            exec('self.tab2.edit' + document_name + ' = LineEdit(' + "\'" + "\'" + ', self.tab2)')
             exec('self.tab2.edit' + document_name + '.setDragEnabled(False)')
             exec('self.tab2.edit' + document_name + '.move(120, ' + str(y_edit) + ')')
             exec('self.tab2.edit' + document_name + '.resize(430, 25)')
-            exec('self.tab2.button' + document_name + ' = QPushButton("Browse", self.tab2)')
-            exec('self.tab2.button' + document_name + '.move(560, ' + str(y_edit) + ')')
-            exec('self.tab2.button' + document_name + '.resize(50, 25)')
-            exec('self.tab2.button' + document_name + '.clicked.connect(self.openFileNameDialog' + document_name + ')')
-
             y_label += 30
-            y_edit  += 30
+            y_edit += 30
 
-        # self.createFunctions()
+        self.tab2.button2 = QPushButton("Start Check", self.tab2)
+        self.tab2.button2.move(250, 400)
+        self.tab2.button2.clicked.connect(self.buttonCheckClicked)
+
+    def buttonCheckClicked(self):
+        for filename in self.list_document:
+            exec("self.correspondences['" + filename + "'] = self.tab2.edit" + filename + ".text()")
+
+        print(self.correspondences)
 
 
 if __name__ == '__main__':
